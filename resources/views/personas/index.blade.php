0af4723fe8ca71a1ebcd3644e2880b32e05b5b39
RubenG79443752 <fab79443752@gmail.com>
2021-02-12 00:17:04 -0400

    pruba1.1

--- resources/views/personas/index.blade.php
+++ resources/views/personas/index.blade.php
@@ -4,4 +4,6 @@ Bienvenidos a la vista index
 <p> Hola </p>
 <p>Adios</p>
 <h2>Aqui la solucion de la plantilla</h2>
-@stop
+@stop 
+<!-- si o si el stop de la seccion -->
+<h1>termina la planilla</h1>
